da91fee5e00517cae38162cbbf0f45ad898c1410
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2025-06-15 16:28:59 +0530

    Create ForEachLoop.java

---
+++ ForEachLoop.java
@@ -0,0 +1,10 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50};
+
+        System.out.println("ARRAY ELEMENTS USING FOR-EACH LOOP:");
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
